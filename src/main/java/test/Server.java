c83b37e3019c3a0db48d622716c50bfa93448867
sistemas <miriamchoyc>
2016-06-06 21:41:29 +0200

    Cambio puerto

--- src/main/java/test/Server.java
+++ src/main/java/test/Server.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Server {
 
     public static void main(String[] args) {
+        System.getProperties().put("server.port", 90);
         SpringApplication.run(Server.class, args);
         System.out.println("Server started correctly.");
     }

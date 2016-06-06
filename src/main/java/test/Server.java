c99ba2696a855bb8b9824d0f180ea6f6d915290d
sistemas <miriamchoyc>
2016-06-06 21:45:28 +0200

    Cambio puerto intento 2

--- src/main/java/test/Server.java
+++ src/main/java/test/Server.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Server {
 
     public static void main(String[] args) {
-        System.getProperties().put("server.port", 90);
+        System.getProperties().put("server.port", 8081);
         SpringApplication.run(Server.class, args);
         System.out.println("Server started correctly.");
     }

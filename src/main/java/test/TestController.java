18ce63ec999f010407be4acdefee301dc213e09d
sistemas <miriamchoyc>
2016-06-06 20:57:22 +0200

    Create application

---
+++ src/main/java/test/TestController.java
@@ -0,0 +1,14 @@
+package test;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+    @RequestMapping("/test")
+    public String index() {
+        return "Hello world!";
+    }
+
+}
